cb3fb26b495f88369b7d0e6658572a71f213b4c5
Simon <wbschxhh@163.com>
2021-05-12 10:04:21 +0800

    refactor: 修改Response Content-Type, IOS设备仅识别该Content-Type才可订阅

--- src/main/java/com/simon/ical/controller/ICalendarController.java
+++ src/main/java/com/simon/ical/controller/ICalendarController.java
@@ -5,7 +5,6 @@ import com.simon.ical.service.ICalendarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +21,7 @@ public class ICalendarController {
     @Autowired
     private ICalendarService iCalendarService;
 
-    @GetMapping(value = "/ical", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    @GetMapping(value = "/ical", produces = "text/calendar; charset=UTF-8")
     public ResponseEntity<FileSystemResource> generate() throws IOException {
         File file = iCalendarService.getOrGenerateIcsContent();
         FileSystemResource fileSystemResource = new FileSystemResource(file);
