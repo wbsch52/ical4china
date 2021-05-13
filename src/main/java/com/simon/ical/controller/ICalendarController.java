9b02506a1b2e102de04dd1dc990fa05918929435
Simon <wbschxhh@163.com>
2021-05-13 10:23:44 +0800

    feat: 增加跨域、统计

--- src/main/java/com/simon/ical/controller/ICalendarController.java
+++ src/main/java/com/simon/ical/controller/ICalendarController.java
@@ -1,25 +1,34 @@
 package com.simon.ical.controller;
 
 import com.simon.ical.service.ICalendarService;
+import com.simon.ical.service.SubscriptionRecordService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
 
 /**
  * @author Simon [xhh52ch@gmail.com]
  */
 @RestController
+@CrossOrigin
 public class ICalendarController {
 
     @Autowired
     private ICalendarService iCalendarService;
+
+    @Autowired
+    private SubscriptionRecordService subscriptionRecordService;
 
     @GetMapping(value = "/ical", produces = "text/calendar; charset=UTF-8")
     public ResponseEntity<FileSystemResource> generate() throws IOException {
@@ -30,5 +39,12 @@ public class ICalendarController {
         return ResponseEntity.ok()
                 .headers(headers)
                 .body(fileSystemResource);
+    }
+
+    @GetMapping(value = "/ical/subscription/count", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Integer>> getSubCount() {
+        Integer count = subscriptionRecordService.count();
+        Map<String, Integer> body = Collections.singletonMap("count", count);
+        return ResponseEntity.ok(body);
     }
 }
